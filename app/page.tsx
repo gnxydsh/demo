62d1ef186413ef2fc814c2f3c92ca91ad117e356
gnxydsh <3153287975@qq.com>
2026-07-29 17:14:35 +0800

    feat: 添加新歌曲资源及歌词

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const items = [
     lrc: "/lrc/Changes-Justin Bieber-歌词.lrc",
   },
   {
-    image: "/p/attention.jpg",
+    image: "/p/Attention.jpg",
     title: "Attention",
     artist: "NewJeans",
     audio: "/m/Attention-NewJeans+(뉴진스).mp3",
@@ -21,6 +21,20 @@ const items = [
     artist: "NewJeans",
     audio: "/m/Ditto-NewJeans+(뉴진스).mp3",
     lrc: "/lrc/Ditto-NewJeans (뉴진스)-歌词.lrc",
+  },
+  {
+    image: "/p/Magnetic.jpg",
+    title: "Magnetic",
+    artist: "ILLIT",
+    audio: "/m/ILLIT-magnetic.mp3",
+    lrc: "/lrc/Magnetic-ILLIT-歌词.lrc",
+  },
+  {
+    image: "/p/圣诞星.jpg",
+    title: "圣诞星",
+    artist: "周杰伦 (feat. 杨瑞代)",
+    audio: "/m/圣诞星+(feat.+杨瑞代)-周杰伦.mp3",
+    lrc: "/lrc/圣诞星 (feat. 杨瑞代)-周杰伦-歌词.lrc",
   },
 ];
 
